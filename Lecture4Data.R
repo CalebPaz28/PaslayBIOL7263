a64a1f7c634eb5ab2112450715b076c65b140335
CalebPaz28 <cap1050@utulsa.edu>
2022-09-29 14:58:43 -0500

    from lecture 4

---
+++ Lecture4Data.R
@@ -0,0 +1,16 @@
+require(tidyverse)
+
+# laod Matt's ebird data from file folder (.csv files are easy to load)
+MBT_ebird <- read_csv("Data/MBT_ebird.csv")
+
+# laod from the website
+MBT_ebird2.csv <- read_csv("https://github.com/mbtoomey/Biol_7263/blob/main/Data/MBT_ebird.csv?raw=true")
+
+MBT_ebird_no_header <- read_csv("Data/MBT_ebird.csv", col_names = FALSE)
+
+# Specify your own column names
+MBT_ebird_no_header <- read.csv("Data/MBT_ebird.csv", col_names = c("first_column","second_column"))
+
+glimpse(MBT_ebird.csv)
+
+head(MBT_ebird.csv)
