1cb7e36eeb0b63b84be89861f81c74057225d913
CalebPaz28 <cap1050@utulsa.edu>
2022-10-08 00:13:35 -0500

    10.7.22 addition of lecture notes

---
+++ 10.7.22 Personal access token.R
@@ -0,0 +1,4 @@
+
+Personal Access token 10.7.22
+
+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
---
+++ Assignments/regular_expressions.R
@@ -0,0 +1,10 @@
+
+
+
+[Link for Assignment 3](https://mbtoomey.github.io/Biol_7263/Assignments/Assignment3.html)
+
+
+
+
+
+
