0c5598072ef0ffd1fed51efb60ba432c6a3d5c29
CalebPaz28 <cap1050@utulsa.edu>
2022-11-09 16:56:03 -0600

    final revisions to presentation

--- Project/rentrez_presentation_notes.R
+++ Project/rentrez_presentation_notes.R
@@ -94,6 +94,8 @@ papers <- sapply(year, search_year, term="alflfa mosaic virus", USE.NAMES = FALS
 plot(year, papers, type='b', main="AMV Publications from 1960 to 2020")
 
 
+####################################################################################################
+
 
 # entrez_summary() less and brief information entrez_fetch() all or most of the information
 
@@ -126,9 +128,157 @@ multi_brca_extract <- extract_from_esummary(brca1_multi_summ, c("title", "pubtyp
 View(multi_brca_extract)
 
 
+brca_data <- data.frame(multi_brca_extract)
+View(brca_data)
+
+#######################################################################################################
+
 #Summary records are useful, but may not include all relevant information
 # We can use entrez_fetch() to get a complete representation
 # We will also use rettype to specify formatting
+
+https://www.ncbi.nlm.nih.gov/books/NBK25499/table/chapter4.T._valid_values_of__retmode_and/
+
+  
+# example of the arguments used
+entrez_fetch(db = "pubmed", id = ..., rettype = "fasta")
+  
+  
+
+# Nucelotide fetch (rettype is more specific and retmode or more general)
+
+all_amv_search <- entrez_fetch(db = "nuccore", id = amv_search$ids, rettype = "fasta")
+class(all_amv_search) 
+
+nchar(all_amv_search) #number of characters
+
+write(all_amv_search, file ="my_amv_transcripts.fasta")
+
+
+# Pubmed fetch
+all_brca_search <- entrez_fetch(db = "pubmed", id = brca_1$ids, rettype = "abstract")
+class(all_brca_search)
+nchar(all_brca_search)
+
+
+write(all_brca_search, file = "my_brca_search_abstracts.fasta")
+
+
+#####################################################################################################
+
+
+# Plot using ggplot2 to plot two differnt searches 
+
+year <- 1950:2022
+alfalfa_search <- glue("alfalfa mosaic virus[TITL]) AND {year}[PDAT]")
+tomato_search <- glue("tomato yellow leaf curl virus[TITL] AND {year}[PDAT]")
+
+search_counts <- tibble(year = year,
+                        alfalfa_search = alfalfa_search, 
+                        tomato_search = tomato_search) %>% 
+  mutate(alfalfa = map_dbl(alfalfa_search, ~entrez_search(db="pubmed",
+                                                          term = .x)$count),
+         tomato = map_dbl(tomato_search, ~entrez_search(db="pubmed",
+                                                        term = .x)$count))
+
+search_counts %>% 
+  select(year, alfalfa, tomato) %>% 
+  pivot_longer(-year) %>% 
+  ggplot(aes(x = year,
+             y = value,
+             group = name,
+             color = name))+
+  geom_line()+
+  geom_smooth()+
+  geom_point(color = "grey")+
+  theme_bw()
+
+
+####################################################################################################
+
+year <- 1950:2022
+amv_search <- glue("alfalfa mosaic virus[TITL] AND {year}[PDAT]")
+all_search <- glue("{year}[PDAT]") #this will include all publications from 1950:2022
+
+
+search_count_1 <- tibble(year = year, 
+                        amv_search = amv_search,
+                        all_search = all_search) %>% 
+  mutate(AMV = map_dbl(amv_search, ~entrez_search(db ="pubmed",
+                                                    term = .x)$count),
+         ALL = map_dbl(all_search, ~entrez_search(db ="pubmed", 
+                                                  term = .x)$count))
+
+
+search_count_1 %>% 
+  select(year, BCTV, ALL) %>%
+  mutate(relative_bctv = BCTV / ALL) %>% 
+  ggplot(aes(x =year, 
+             y = relative_bctv)) +
+  theme_bw()+
+  geom_line()
+
+
+search_count_1 %>% 
+  select(year, BCTV, ALL) %>%
+  mutate(relative_bctv = 100 * BCTV / ALL) %>% 
+  ggplot(aes(x =year, 
+             y = relative_bctv)) +
+  theme_bw()+
+  geom_line()
+
+
+
+
+#########################################################################################################
+# All pulications in comparison to a topic of choice
+
+year <- 1950:2022
+amv_search <- glue("alfalfa mosaic virus[TITL] AND {year}[PDAT]")
+all_virus_search <- glue("viruses AND {year}[PDAT]") #this will include all publications on viruses from 1950:2022
+
+
+search_count_2 <- tibble(year = year, 
+                        amv_search = amv_search,
+                        all_virus_search = all_virus_search) %>% 
+  mutate(AMV = map_dbl(amv_search, ~entrez_search(db ="pubmed",
+                                                    term = .x)$count),
+         ALL_virus = map_dbl(all_virus_search, ~entrez_search(db ="pubmed", 
+                                                  term = .x)$count))
+
+
+search_count_2 %>% 
+  select(year, AMV, ALL_virus) %>% 
+  pivot_longer(-year) %>% 
+  ggplot(aes(x = year,
+             y = value, 
+             group = name, 
+             color = name)) +
+  theme_bw() + 
+  geom_line(size = 1)
+
+
+search_count_2 %>% 
+  select(year, AMV, ALL_virus) %>%
+  mutate(relative_amv = AMV / ALL_virus * 100) %>% 
+  ggplot(aes(x =year, 
+             y = relative_amv)) +
+  theme_bw()+
+  geom_line(size = 1)
+
+
+#This gives us the relative number of publications of AMV in comparision to ALL virus publications from 1950:2022 
+
+###########################################################################################################
+
+#Excercises 
+
+# 1. Create a search of a organism or set of organisms. 
+# 2. Use the summary or fetch arguement to extract relavent information to your search. 
+# 3. Create a plot to see how this topic has been reported on over a particular time span. 
+
+
+
 
 
 
