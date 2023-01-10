f9f6e774f6cb2e2de91d488bd75dcde3672ec930
CalebPaz28 <cap1050@utulsa.edu>
2023-01-10 17:21:04 -0600

    changes to plots

--- Project/ali_plots/pepper_project.R
+++ Project/ali_plots/pepper_project.R
@@ -1,5 +1,7 @@
 
 library(rentrez)
+library(glue)
+library(tidyverse)
 # Pepper virus search 
 pepper_virus_search <- entrez_search(db = "pubmed", term = "(pepper viruses OR capsicum viruses) AND 1950:2022[PDAT]")
 pepper_virus_search
@@ -61,7 +63,7 @@ novel_search
 
 ## Plot novel plant virus reports
 
-year <- 1950:2020
+year <- 2000:2022
 novel_virus_search <- glue("(novel plant viruses OR undescribed plant viruses) AND {year}[PDAT]")
 all_plant_counts <- tibble(year = year,
                            novel_virus_search = novel_virus_search) %>% 
@@ -87,7 +89,7 @@ novel_plot
 ngs_i_search <- entrez_search(db = "pubmed", term = "(NGS plant virus OR next generation sequencing plant virus) AND 1950:2020[PDAT]")
 ngs_i_search
 
-year <- 1950:2020
+year <- 2000:2022
 ngs_search <- glue("(NGS plant virus OR next generation sequencing plant virus) AND {year}[PDAT]")
 ngs_search_counts <- tibble(year = year,
                             ngs_search = ngs_search) %>% 
@@ -106,6 +108,39 @@ ngs_plot <- ngs_search_counts %>%
   theme_bw()
 
 ngs_plot
+
+#How to save a plot from ggplot2 (it will autosave the most recent plot)
+ggsave("ngs_plot.png")
+
+
+library(patchwork)
+combined_plot <- novel_plot / ngs_plot / pmmov_plot
+
+combined_plot
+
+# Pepper mild mottle virus search plot
+year <- 2000:2022
+ngs_search <- glue("Pepper mild mottle virus AND {year}[PDAT]")
+ngs_search_counts <- tibble(year = year,
+                            ngs_search = ngs_search) %>% 
+  mutate(ngs_search = map_dbl(ngs_search, ~entrez_search(db="pubmed",term = .x)$count))
+
+pmmov_plot <- ngs_search_counts %>% 
+  select(year,ngs_search) %>% 
+  pivot_longer(-year) %>% 
+  ggplot(aes(x = year,
+             y = value,
+             group = name,
+             color = name))+
+  geom_line(size = 1)+
+  geom_smooth(size =1)+
+  geom_point(color = "black")+
+  theme_bw()
+
+pmmov_plot
+
+
+
 
 
 install.packages("patchwork")
